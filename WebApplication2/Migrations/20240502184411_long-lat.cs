c6fe5174c3bea5be6933d8b5bd699c84bc67721f
Ibrahim Gad <yehya2154@gmail.com>
2024-05-02 22:48:38 +0300

    add long and lat

---
+++ WebApplication2/Migrations/20240502184411_long-lat.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication2.Migrations
+{
+    /// <inheritdoc />
+    public partial class longlat : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Lat1",
+                table: "Rides",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Lat2",
+                table: "Rides",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Long1",
+                table: "Rides",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Long2",
+                table: "Rides",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Lat1",
+                table: "Rides");
+
+            migrationBuilder.DropColumn(
+                name: "Lat2",
+                table: "Rides");
+
+            migrationBuilder.DropColumn(
+                name: "Long1",
+                table: "Rides");
+
+            migrationBuilder.DropColumn(
+                name: "Long2",
+                table: "Rides");
+        }
+    }
+}
